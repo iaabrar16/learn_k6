10b0a991c246b5f345019f93429a8c736bbb68c1
Ismail Aabrar <kingaabrar@gmail.com>
2024-11-13 02:54:47 +0600

    get api testing topic done

---
+++ get-api.js
@@ -0,0 +1,29 @@
+import http from 'k6/http'
+import {check} from 'k6'
+
+export const options={
+    vus: 10,
+    iterations:20
+};
+
+const params={
+    headers:{
+        'Authorization': 'Bearer 737433476'
+
+    }
+}
+
+let headers_api={
+    'Authorization': 'Bearer 535534'
+}
+
+const url="https://gorest.co.in/public/v2/users"
+
+export default function(){
+    
+    const response = http.get(url, {headers:headers_api})
+
+    check(response,{
+        "Status code validation": (response)=>response.status===200
+    })
+}
